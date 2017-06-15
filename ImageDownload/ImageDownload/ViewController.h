bc47339a0f44d6a6fc34b149e92e26e734374a9d
tldrd0117 <tldrd0117@gmail.com>
2017-06-15 21:53:23 +0900

    Section16 start
    이미지 비동기 다운로드

---
+++ ImageDownload/ImageDownload/ViewController.h
@@ -0,0 +1,22 @@
+//
+//  ViewController.h
+//  ImageDownload
+//
+//  Created by 이성재 on 2017. 6. 15..
+//  Copyright © 2017년 이성재. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *imgView;
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (strong, nonatomic) IBOutlet UIProgressView *progressView;
+
+- (IBAction)downloadAction:(id)sender;
+- (IBAction)suspendAction:(id)sender;
+- (IBAction)resumeAction:(id)sender;
+- (IBAction)cancelAction:(id)sender;
+
+@end
+
---
+++ ImageDownload/ImageDownload/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  ImageDownload
+//
+//  Created by 이성재 on 2017. 6. 15..
+//  Copyright © 2017년 이성재. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)downloadAction:(id)sender {
+}
+
+- (IBAction)suspendAction:(id)sender {
+}
+
+- (IBAction)resumeAction:(id)sender {
+}
+
+- (IBAction)cancelAction:(id)sender {
+}
+@end
